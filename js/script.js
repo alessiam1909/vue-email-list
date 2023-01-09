ea1e37b4d13fb3916e67a65e7741d822d1799757
alessiam1909 <ale.menon.vr@gmail.com>
2023-01-09 17:09:22 +0100

    esercizio base

--- js/script.js
+++ js/script.js
@@ -5,10 +5,17 @@ const {
 createApp({
     data(){
         return{
-
+            emailList: []
         }
     },
     mounted(){
+
+            for(let i=0; i<10; i++){
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((risultato) => {
+                    this.email = risultato.data.response
+                    this.emailList.push(this.email)
+                })
+            }
         
     }
-})
+}).mount('#app')
