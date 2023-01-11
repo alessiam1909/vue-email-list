7d18070a1745e3e65532fcbd201fbb5a6ba355aa
alessiam1909 <ale.menon.vr@gmail.com>
2023-01-11 15:29:00 +0100

    sistemazione errore js

--- js/script.js
+++ js/script.js
@@ -3,19 +3,19 @@ const {
 } = Vue
 
 createApp({
-    data(){
-        return{
+    data() {
+        return {
             emailList: []
         }
     },
-    mounted(){
+    mounted() {
 
-            for(let i=0; i<10; i++){
-                axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((risultato) => {
-                    this.email = risultato.data.response
-                    this.emailList.push(this.email)
-                })
-            }
-        
+        for (let i = 0; i < 10; i++) {
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((risultato) => {
+                email = risultato.data.response
+                this.emailList.push(email)
+            })
+        }
+
     }
 }).mount('#app')
